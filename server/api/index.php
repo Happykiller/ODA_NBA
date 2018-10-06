6aa6fdc754012d32d5bb5bd2a2b399402c482bde
fabrice.rosito@atbq.fr <fabrice.rosito@atbq.fr>
2018-10-06 14:38:48 +0200

    update

--- server/api/index.php
+++ server/api/index.php
@@ -6,17 +6,21 @@ require '../header.php';
 require "../vendor/autoload.php";
 require '../config/config.php';
 
-use cebe\markdown\GithubMarkdown;
-use Slim\Slim;
-use \stdClass, \Oda\SimpleObject\OdaPrepareInterface, \Oda\SimpleObject\OdaPrepareReqSql, \Oda\OdaLibBd;
-use Oda\OdaRestInterface;
+use 
+    stdClass,
+    Slim\Slim,
+    Oda\OdaLibBd,
+    cebe\markdown\GithubMarkdown,
+    Oda\OdaRestInterface,
+    Oda\SimpleObject\OdaPrepareInterface,
+    Oda\SimpleObject\OdaPrepareReqSql
+;
 
 $slim = new Slim();
 //--------------------------------------------------------------------------
 
 $slim->notFound(function () use ($slim) {
-    $params = new OdaPrepareInterface();
-    $params->slim = $slim;
+    $params = new OdaPrepareInterface($slim);
     $INTERFACE = new OdaRestInterface($params);
     $INTERFACE->dieInError('not found');
 });
@@ -31,49 +35,43 @@ $slim->get('/', function () {
 // MATCH
 
 $slim->get('/match/', function () use ($slim) {
-    $params = new OdaPrepareInterface();
-    $params->slim = $slim;
+    $params = new OdaPrepareInterface($slim);
     $INTERFACE = new MatchInterface($params);
     $INTERFACE->getAll();
 });
 
 $slim->get('/match/:id', function ($id) use ($slim) {
-    $params = new OdaPrepareInterface();
-    $params->slim = $slim;
+    $params = new OdaPrepareInterface($slim);
     $INTERFACE = new MatchInterface($params);
     $INTERFACE->get($id);
 });
 
 $slim->post('/match/', function () use ($slim) {
-    $params = new OdaPrepareInterface();
+    $params = new OdaPrepareInterface($slim);
     $params->arrayInput = array("teamA", "colorA", "teamB", "colorB");
     $params->modePublic = false;
-    $params->slim = $slim;
     $INTERFACE = new MatchInterface($params);
     $INTERFACE->create();
 });
 
 $slim->get('/match/:id/report/recap/', function ($id) use ($slim) {
-    $params = new OdaPrepareInterface();
-    $params->slim = $slim;
+    $params = new OdaPrepareInterface($slim);
     $INTERFACE = new MatchInterface($params);
     $INTERFACE->getRecapForMatch($id);
 });
 
 $slim->post('/match/event/', function () use ($slim) {
-    $params = new OdaPrepareInterface();
+    $params = new OdaPrepareInterface($slim);
     $params->arrayInput = array("matchId","team");
     $params->arrayInputOpt = array("twoMissing"=>0,"twoSuccess"=>0,"treeMissing"=>0,"treeSuccess"=>0,"oneMissing"=>0,"oneSuccess"=>0,"fault"=>0,"lost"=>0,"steal"=>0);
     $params->modePublic = false;
-    $params->slim = $slim;
     $INTERFACE = new MatchInterface($params);
     $INTERFACE->createEvent();
 });
 
 $slim->delete('/match/:id/event/', function ($id) use ($slim) {
-    $params = new OdaPrepareInterface();
+    $params = new OdaPrepareInterface($slim);
     $params->modePublic = false;
-    $params->slim = $slim;
     $INTERFACE = new MatchInterface($params);
     $INTERFACE->undoEvent($id);
 });
